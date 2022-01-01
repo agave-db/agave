8ee122e65cd05a04625cdd8756042c511e1ba7d4
Jamie Gaskins <jgaskins@hey.com>
2022-01-01 16:25:04 -0500

    Fix TOGGLE command for keys that don't exist yet

--- src/commands/toggle.cr
+++ src/commands/toggle.cr
@@ -1,12 +1,25 @@
 require "../commands"
 
 Agave::Commands.define toggle do
-  case value = data[key]?
-  when true
-    data[key] = false
-  when false
-    data[key] = true
+  if new_value = command[2]?.as?(String | Bool)
+    data[key] = case new_value
+                when Bool
+                  new_value
+                when "true", "t"
+                  true
+                when "false", "f"
+                  false
+                else
+                  return ClientError.new("ERR", "The new value passed to TOGGLE must be a boolean or a string value that can be parsed as a boolean")
+                end
   else
-    ClientError.new("WRONGTYPE", "Value in #{key} must be a boolean")
+    case value = data[key]?
+    when true
+      data[key] = false
+    when false, nil
+      data[key] = true
+    else
+      ClientError.new("WRONGTYPE", "Value in #{key} must be a boolean")
+    end
   end
 end
